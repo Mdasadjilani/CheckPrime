f99fa6a3774088c5ee97b07973e7e4f6be8fc137
Mdasadjilani <asadjilani20@gmail.com>
2022-09-10 19:25:31 +0530

    CheckPrime

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPrime
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            FindPrimeNumber(n);
+        }
+        public static void FindPrimeNumber(int n)
+        {
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Not Prime");
+            }
+            else
+            {
+                Console.WriteLine("prime");
+            }
+        }
+    }
+}
